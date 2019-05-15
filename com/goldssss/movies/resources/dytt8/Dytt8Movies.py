b7efab625ba6c253cc2b6ee1c9df4c863e89655a
majinxin1 <majinxin1@jd.com>
2019-05-15 21:02:10 +0800

    编码问题

--- com/goldssss/movies/resources/dytt8/Dytt8Movies.py
+++ com/goldssss/movies/resources/dytt8/Dytt8Movies.py
@@ -1,10 +1,8 @@
 import re
 import urllib3
 from bs4 import BeautifulSoup
-
 from com.goldssss.movies.config import MoviesConfig
 from com.goldssss.movies.resources.SelectMovies import SelectMovies
-
 
 class Dytt8(SelectMovies):
     url = "http://s.ygdy8.com/plus/so.php?typeid=1&"
@@ -14,10 +12,11 @@ class Dytt8(SelectMovies):
         url = MoviesConfig.dytt8Url + urllib3.util.url.quote(name.encode(MoviesConfig.dytt8UrlEncoding))
         print('ddty select open url >>>' + url)
         urllib3.disable_warnings()
-        dyttResponse = urllib3.PoolManager().request(MoviesConfig.httpRequestGet,url)
-        print(dyttResponse.data)
-        soup = BeautifulSoup(dyttResponse)
+        dyttResponse = urllib3.PoolManager().request(MoviesConfig.httpRequestGet,url,headers={})
+        soup = BeautifulSoup(dyttResponse.data)
         hrefs = soup.find_all('a')
         for a in hrefs:
             print(a)
-        print(dyttResponse.data)
+
+ddty = Dytt8()
+ddty.selectMovies(name='复仇者')
